85af13a144fcf5487b748bcb41d774b2b4a8b84d
Jhonatan Mota <jhonatanmotadev@gmail.com>
2025-09-22 17:55:28 -0300

    Update firebase.ts

--- app/services/firebase.ts
+++ app/services/firebase.ts
@@ -1,9 +1,2 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBCbkn62UvT9Zs9T7hA0qEIgEQO4Jk4TyI",
-  authDomain: "sportsconnect-c2fe4.firebaseapp.com",
-  projectId: "sportsconnect-c2fe4",
-  storageBucket: "sportsconnect-c2fe4.firebasestorage.app",
-  messagingSenderId: "1095499496218",
-  appId: "1:1095499496218:web:a9fa6543256ca747b60aa3",
-  measurementId: "G-TT032HZP51"
 };
